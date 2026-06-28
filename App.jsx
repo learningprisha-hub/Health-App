785d3e7d0a3d95a8f5949a604a39ae2d61a5334e
learningprisha-hub <learningprisha@gmail.com>
2026-06-28 12:54:56 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Nav from "./components/Navebar"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    // <>
+    <Nav/>
+          // </>
+  )
+}
+
+export default App
---
+++ Navebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+  return (
+  <nav className="navbar">
+<div className='nav-brand'>
+<span>✚</span>
+<span>Health Companion</span>
+</div>
+<ul className="nav-links">
+    <li><a href="#home">home</a></li>
+<li><a href="#home">Dashboard</a></li>
+<li><a href="#home">Symptons</a></li>
+<li><a href="#home">Profile</a></li>
+</ul>
+  </nav>
+
+  )
+}
+
+export default Nav
